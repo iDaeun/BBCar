d74044057e1cea0546063ff64346acca60cadde8
iDaeun <47049072+ekdms2309@users.noreply.github.com>
2019-09-24 12:40:27 +0900

    [ycar-PassengerBoot] Spring Boot 프로젝트 생성

---
+++ ycar-PassengerBoot/src/test/java/com/ycar/passenger/YcarPassengerBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ycar.passenger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YcarPassengerBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
